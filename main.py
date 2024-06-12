783f0bd2cfd220c81021eee1ffc5819b4fff34a5
Prasath <prasathrk1407@gmail.com>
2024-06-12 13:18:34 +0530

    changes in feature3

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 def check():
     print("I am calling")
-
-def feature1():
-    print("feature-1 is implemeted")
-
-def feature2():
-    print("feature-2 is implemeted")
-    
+  
 def feature3():
-    print("feature-3 is implemeted")
+    print("feature-3 is just created")
     
 if __name__=='__main__':
     check()
-    feature1()
-    feature2()
     feature3()
