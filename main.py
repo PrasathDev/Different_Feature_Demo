70479d79fc07edb8ca7c1060b9e9cc2c41deceb3
Prasath <prasathrk1407@gmail.com>
2024-06-12 11:50:56 +0530

    implement feature3

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def feature1():
 def feature2():
     print("feature-2 is implemeted")
     
+def feature3():
+    print("feature-3 is implemeted")
+    
 if __name__=='__main__':
     check()
     feature1()
     feature2()
+    feature3()
