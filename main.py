3117e40dc86d63d43f42afe4da79f00df4aaa1a7
Prasath <prasathrk1407@gmail.com>
2024-06-12 12:55:17 +0530

    added vote eligibility logic

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def check():
     print("I am calling")
 
-def feature1():
+def feature1(age):
     print("feature-1 is enabled")
     print("additional part added in feature1")
+    if(age>18):
+        print("eligible to vote")
     
 if __name__=='__main__':
     check()
-    feature1()
+    age=22
+    feature1(age)
