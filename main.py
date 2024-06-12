722c0601a193c8063b10209c5272a6e55880ea80
Prasath <prasathrk1407@gmail.com>
2024-06-12 11:36:45 +0530

    implement feature2

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def check():
     print("I am calling")
 
 def feature1():
-    print("feature-1 is enabled")
+    print("feature-1 is implemeted")
+
+def feature2():
+    print("feature-2 is implemeted")
     
 if __name__=='__main__':
     check()
     feature1()
+    feature2()
